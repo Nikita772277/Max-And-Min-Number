bdeea054ac602c3dd2d4912ae925428b70a4c90f
Nikita77227 <Nikita77227@DESKTOP-8NU1P3S>
2022-12-25 20:35:34 +0300

    Добавьте файлы проекта.

---
+++ Max-And-Min-Number/Checks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_And_Min_Number
+{
+    internal class Checks
+    {
+        public string SearchForExtremeValues(string number)
+        {
+            if (string.IsNullOrEmpty(number))
+                return "Строка пуста";
+            var split= number.Split(' ',',');
+            bool checkFirstPosition = int.TryParse(split[0], out int resultFirstPosition);
+            int maxNumber = resultFirstPosition, minNumber = resultFirstPosition;
+            for(int i = 0; i < split.Length; i++)
+            {
+                bool check = int.TryParse(split[i], out int result);
+                if (maxNumber < result)
+                    maxNumber = result;
+                if (minNumber > result)
+                    minNumber = result;
+            }
+            return $"Максимальное число: {maxNumber}\nМинимальное число: {minNumber}";
+        }
+    }
+}
---
+++ Max-And-Min-Number/Program.cs
@@ -0,0 +1,6 @@
+﻿using Max_And_Min_Number;
+using System;
+
+Checks checks= new();
+var result= checks.SearchForExtremeValues("-25");
+Console.WriteLine(result);
